81d7bdf85108696ba05c45b2641f9edb00f2923f
qqytqqyt <ytong007@hotmail.com>
2020-09-12 16:18:19 +0100

    ability to retrieve spell tooptips data

--- QuestTextRetriever/QuestTextRetriever/Program.cs
+++ QuestTextRetriever/QuestTextRetriever/Program.cs
@@ -10,11 +10,14 @@ namespace QuestTextRetriever
     {
         static void Main(string[] args)
         {
-            var retriever = new Retriever(@"D:\qqytqqyt\Documents\OwnProjects\Translator\686500-Wow-ChineseDB-master\Wow-ChineseDB\quest_template.sql", @"D:\qqytqqyt\Documents\OwnProjects\Translator\template.txt");
-            retriever.Execute(@"D:\qqytqqyt\Documents\OwnProjects\Translator\output.txt");
+            var spellReader = new SpellReader();
+            spellReader.Write(@"C:\Users\qqytqqyt\OneDrive\Documents\OneDrive\OwnProjects\WoWTranslator\Data\output.txt");
 
-            var htmlRetriever = new HtmlRetriever(@"C:\Users\qqytqqyt\OneDrive\Documents\OneDrive\OwnProjects\WoWTranslator\WowHead\", @"D:\qqytqqyt\Documents\OwnProjects\Translator\template.txt");
-            htmlRetriever.ExecuteJson(@"D:\qqytqqyt\Documents\OwnProjects\Translator\outputretail.txt");
+            //var retriever = new Retriever(@"D:\qqytqqyt\Documents\OwnProjects\Translator\686500-Wow-ChineseDB-master\Wow-ChineseDB\quest_template.sql", @"D:\qqytqqyt\Documents\OwnProjects\Translator\template.txt");
+            //retriever.Execute(@"D:\qqytqqyt\Documents\OwnProjects\Translator\output.txt");
+
+            //var htmlRetriever = new HtmlRetriever(@"C:\Users\qqytqqyt\OneDrive\Documents\OneDrive\OwnProjects\WoWTranslator\WowHead\", @"D:\qqytqqyt\Documents\OwnProjects\Translator\template.txt");
+            //htmlRetriever.ExecuteJson(@"D:\qqytqqyt\Documents\OwnProjects\Translator\outputretail.txt");
         }
     }
 }
