7219ac1df600b18b5d0325e0debeb1cbe7df1be5
qqytqqyt <ytong007@hotmail.com>
2023-01-21 21:06:13 +0000

    [Scanner] New scanner project

---
+++ WoWeuCN_Scanner/TooltipsData_cn.lua
@@ -0,0 +1,4 @@
+﻿WoWeuCN_Scanner_base = "3.4.1.47612";   -- version
+WoWeuCN_Scanner_date = "2023-01-17"; -- date of creation base
+
+WoWeuCN_Scanner_lang = "CN";      -- language
---
+++ WoWeuCN_Scanner/TooltipsScanner.lua
@@ -0,0 +1,360 @@
+-- wait functions from QTR
+local WoWeuCN_Scanner_waitFrame = nil;
+local WoWeuCN_Scanner_waitTable = {};
+
+function WoWeuCN_Scanner_ScanClear()
+    WoWeuCN_Scanner_SpellToolIndex = 1;
+    WoWeuCN_Scanner_SpellToolTips0 = {} 
+    WoWeuCN_Scanner_SpellToolTips100000 = {} 
+    WoWeuCN_Scanner_SpellToolTips200000 = {} 
+    WoWeuCN_Scanner_SpellToolTips300000 = {}      
+    WoWeuCN_Scanner_ItemToolTips0 = {} 
+    WoWeuCN_Scanner_ItemToolTips100000 = {} 
+    WoWeuCN_Scanner_ItemIndex = 1
+    WoWeuCN_Scanner_UnitToolTips0 = {} 
+    WoWeuCN_Scanner_UnitIndex = 1
+    WoWeuCN_Scanner_Achivements0 = {} 
+    WoWeuCN_Scanner_AchivementsIndex = 1
+    print("Clear");
+end
+
+function WoWeuCN_Scanner_ScanInit()  
+  if (WoWeuCN_Scanner_SpellToolTips0 == nil) then
+  WoWeuCN_Scanner_SpellToolTips0 = {} 
+  end
+  if (WoWeuCN_Scanner_SpellToolTips100000 == nil) then
+  WoWeuCN_Scanner_SpellToolTips100000 = {} 
+  end
+  if (WoWeuCN_Scanner_SpellToolTips200000 == nil) then
+  WoWeuCN_Scanner_SpellToolTips200000 = {} 
+  end
+  if (WoWeuCN_Scanner_SpellToolTips300000 == nil) then
+  WoWeuCN_Scanner_SpellToolTips300000 = {} 
+  end
+  if (WoWeuCN_Scanner_SpellToolTips400000 == nil) then
+  WoWeuCN_Scanner_SpellToolTips400000 = {} 
+  end
+  if (WoWeuCN_Scanner_SpellToolIndex == nil) then
+  WoWeuCN_Scanner_SpellToolIndex = 1
+  end
+
+  if (WoWeuCN_Scanner_UnitToolTips0 == nil) then
+  WoWeuCN_Scanner_UnitToolTips0 = {} 
+  end
+  if (WoWeuCN_Scanner_UnitToolTips100000 == nil) then
+  WoWeuCN_Scanner_UnitToolTips100000 = {} 
+  end
+  if (WoWeuCN_Scanner_UnitToolTips200000 == nil) then
+  WoWeuCN_Scanner_UnitToolTips200000 = {} 
+  end
+  if (WoWeuCN_Scanner_UnitIndex == nil) then
+  WoWeuCN_Scanner_UnitIndex = 1
+  end
+
+  if (WoWeuCN_Scanner_ItemToolTips0 == nil) then
+  WoWeuCN_Scanner_ItemToolTips0 = {} 
+  end
+  if (WoWeuCN_Scanner_ItemToolTips100000 == nil) then
+  WoWeuCN_Scanner_ItemToolTips100000 = {} 
+  end
+  if (WoWeuCN_Scanner_ItemToolTips200000 == nil) then
+  WoWeuCN_Scanner_ItemToolTips200000 = {} 
+  end
+  if (WoWeuCN_Scanner_ItemIndex == nil) then
+  WoWeuCN_Scanner_ItemIndex = 1
+  end
+  
+  if (WoWeuCN_Scanner_Achivements0 == nil) then
+    WoWeuCN_Scanner_Achivements0 = {} 
+  end
+  if (WoWeuCN_Scanner_AchivementsIndex == nil) then
+    WoWeuCN_Scanner_AchivementsIndex = 1
+  end
+
+  if (WoWeuCN_Scanner_QuestToolTips == nil) then
+    WoWeuCN_Scanner_QuestToolTips = {} 
+  end
+  if (WoWeuCN_Scanner_QuestIndex == nil) then
+    WoWeuCN_Scanner_QuestIndex = 1
+  end
+end
+
+function WoWeuCN_Scanner_ScanIndex(index)
+    WoWeuCN_Scanner_SpellToolIndex = tonumber(index);
+    WoWeuCN_Scanner_ItemIndex = tonumber(index);
+    WoWeuCN_Scanner_UnitIndex = tonumber(index);
+    WoWeuCN_Scanner_AchivementsIndex = tonumber(index);
+    WoWeuCN_Scanner_QuestIndex = tonumber(index);
+    print(index)
+end
+
+function WoWeuCN_Scanner_wait(delay, func, ...)
+  if(type(delay)~="number" or type(func)~="function") then
+    return false;
+  end
+  if (WoWeuCN_Scanner_waitFrame == nil) then
+    WoWeuCN_Scanner_waitFrame = CreateFrame("Frame","WoWeuCN_Scanner_waitFrame", UIParent);
+    WoWeuCN_Scanner_waitFrame:SetScript("onUpdate",function (self,elapse)
+      local count = #WoWeuCN_Scanner_waitTable;
+      local i = 1;
+      while(i<=count) do
+        local waitRecord = tremove(WoWeuCN_Scanner_waitTable,i);
+        local d = tremove(waitRecord,1);
+        local f = tremove(waitRecord,1);
+        local p = tremove(waitRecord,1);
+        if(d>elapse) then
+          tinsert(WoWeuCN_Scanner_waitTable,i,{d-elapse,f,p});
+          i = i + 1;
+        else
+          count = count - 1;
+          f(unpack(p));
+        end
+      end
+    end);
+  end
+  tinsert(WoWeuCN_Scanner_waitTable,{delay,func,{...}});
+  return true;
+end
+
+function WoWeuCN_Scanner_ScanSpellAuto(startIndex, attempt, counter)
+  if (startIndex > 400000) then
+    return;
+  end
+  for i = startIndex, startIndex + 150 do
+    qcInformationTooltip:SetOwner(UIParent, "ANCHOR_NONE")
+    qcInformationTooltip:ClearLines()
+    qcInformationTooltip:SetHyperlink('spell:' .. i)
+    qcInformationTooltip:Show()
+    local text =  EnumerateTooltipStyledLines(qcInformationTooltip)
+    if (text ~= '' and text ~= nil) then
+      if (i >=0 and i < 100000) then
+        if (WoWeuCN_Scanner_SpellToolTips0[i .. ''] == nil or string.len(WoWeuCN_Scanner_SpellToolTips0[i .. '']) < string.len(text)) then
+          WoWeuCN_Scanner_SpellToolTips0[i .. ''] = text
+        end
+      elseif (i >=100000 and i < 200000) then
+        if (WoWeuCN_Scanner_SpellToolTips100000[i .. ''] == nil or string.len(WoWeuCN_Scanner_SpellToolTips100000[i .. '']) < string.len(text)) then
+          WoWeuCN_Scanner_SpellToolTips100000[i .. ''] = text
+        end
+      elseif (i >=200000 and i < 300000) then
+        if (WoWeuCN_Scanner_SpellToolTips200000[i .. ''] == nil or string.len(WoWeuCN_Scanner_SpellToolTips200000[i .. '']) < string.len(text)) then
+          WoWeuCN_Scanner_SpellToolTips200000[i .. ''] = text
+        end
+      elseif (i >=300000 and i < 400000) then
+        if (WoWeuCN_Scanner_SpellToolTips300000[i .. ''] == nil or string.len(WoWeuCN_Scanner_SpellToolTips300000[i .. '']) < string.len(text)) then
+          WoWeuCN_Scanner_SpellToolTips300000[i .. ''] = text
+        end
+      end
+      print(i)
+    end
+  end
+  print(attempt)
+  print(counter)
+  WoWeuCN_Scanner_SpellToolIndex = startIndex
+  if (counter >= 5) then
+    WoWeuCN_Scanner_wait(0.5, WoWeuCN_Scanner_ScanSpellAuto, startIndex + 150, attempt + 1, 0)
+  else
+    WoWeuCN_Scanner_wait(0.5, WoWeuCN_Scanner_ScanSpellAuto, startIndex, attempt + 1, counter + 1)
+  end
+end
+
+function WoWeuCN_Scanner_ScanUnitAuto(startIndex, attempt, counter)
+  if (startIndex > 200000) then
+    return;
+  end
+  for i = startIndex, startIndex + 250 do
+    qcInformationTooltip:SetOwner(UIParent, "ANCHOR_NONE")
+    qcInformationTooltip:ClearLines()
+    local guid = "Creature-0-0-0-0-"..i.."-0000000000";
+    qcInformationTooltip:SetHyperlink('unit:' .. guid)
+    qcInformationTooltip:Show()
+    local text =  EnumerateTooltipStyledLines(qcInformationTooltip)
+    if (text ~= '' and text ~= nil) then
+     if (i >=0 and i < 100000) then
+      if (WoWeuCN_Scanner_UnitToolTips0[i .. ''] == nil or string.len(WoWeuCN_Scanner_UnitToolTips0[i .. '']) < string.len(text)) then
+        WoWeuCN_Scanner_UnitToolTips0[i .. ''] = text
+      end
+    elseif (i >=100000 and i < 200000) then
+      if (WoWeuCN_Scanner_UnitToolTips100000[i .. ''] == nil or string.len(WoWeuCN_Scanner_UnitToolTips100000[i .. '']) < string.len(text)) then
+        WoWeuCN_Scanner_UnitToolTips100000[i .. ''] = text
+      end
+    end
+    end
+    print(i)
+  end
+  print(attempt)
+  print(counter)
+  WoWeuCN_Scanner_UnitIndex = startIndex
+  if (counter >= 3) then
+    WoWeuCN_Scanner_wait(0.5, WoWeuCN_Scanner_ScanUnitAuto, startIndex + 250, attempt + 1, 0)
+  else
+    WoWeuCN_Scanner_wait(0.5, WoWeuCN_Scanner_ScanUnitAuto, startIndex, attempt + 1, counter + 1)
+  end
+end
+
+function WoWeuCN_Scanner_ScanItemAuto(startIndex, attempt, counter)
+  if (startIndex > 300000) then
+    return;
+  end
+  for i = startIndex, startIndex + 150 do
+    local itemType, itemSubType, _, _, _, _, classID, subclassID = select(6, GetItemInfo(i))
+    if (classID~=nil) then
+      qcInformationTooltip:SetOwner(UIParent, "ANCHOR_NONE")
+      qcInformationTooltip:ClearLines()
+      qcInformationTooltip:SetHyperlink('item:' .. i .. ':0:0:0:0:0:0:0')
+      qcInformationTooltip:Show()
+      local text = EnumerateTooltipStyledLines(qcInformationTooltip)
+      text = text .. '{{{' .. classID .. '}}}'
+      if (text ~= '' and text ~= nil) then
+        if (i >=0 and i < 100000) then
+          if (WoWeuCN_Scanner_ItemToolTips0[i .. ''] == nil or string.len(WoWeuCN_Scanner_ItemToolTips0[i .. '']) < string.len(text)) then
+            WoWeuCN_Scanner_ItemToolTips0[i .. ''] = text
+          end
+        elseif (i >=100000 and i < 200000) then
+          if (WoWeuCN_Scanner_ItemToolTips100000[i .. ''] == nil or string.len(WoWeuCN_Scanner_ItemToolTips100000[i .. '']) < string.len(text)) then
+            WoWeuCN_Scanner_ItemToolTips100000[i .. ''] = text
+          end
+          print(i)
+        end
+      end
+    else
+      if (classId==nil) then
+        print(i .. " skip")
+      else
+        print(i .. " gear")
+      end
+    end
+  end
+  print(attempt)
+  print(counter)
+  WoWeuCN_Scanner_ItemIndex = startIndex
+  if (counter >= 5) then
+    WoWeuCN_Scanner_wait(0.5, WoWeuCN_Scanner_ScanItemAuto, startIndex + 150, attempt + 1, 0)
+  else
+    WoWeuCN_Scanner_wait(0.5, WoWeuCN_Scanner_ScanItemAuto, startIndex, attempt + 1, counter + 1)
+  end
+end
+
+function WoWeuCN_Scanner_ScanAchivementAuto(startIndex, attempt, counter)
+  if (startIndex > 30000) then
+    return;
+  end
+  for i = startIndex, startIndex + 150 do
+    qcInformationTooltip:SetOwner(UIParent, "ANCHOR_NONE")
+    qcInformationTooltip:ClearLines()
+    qcInformationTooltip:SetHyperlink('achievement:' .. i .. ':0:0:0:0:0:0:0:0')
+    qcInformationTooltip:Show()
+    local text = EnumerateTooltipStyledLines(qcInformationTooltip)
+    if (text ~= '' and text ~= nil) then
+      if (WoWeuCN_Scanner_Achivements0[i .. ''] == nil or string.len(WoWeuCN_Scanner_Achivements0[i .. '']) < string.len(text)) then
+        WoWeuCN_Scanner_Achivements0[i .. ''] = text
+      end
+    end
+  end
+  print(attempt)
+  print(counter)
+  WoWeuCN_Scanner_AchivementsIndex = startIndex
+  if (counter >= 5) then
+    WoWeuCN_Scanner_wait(0.5, WoWeuCN_Scanner_ScanAchivementAuto, startIndex + 150, attempt + 1, 0)
+  else
+    WoWeuCN_Scanner_wait(0.5, WoWeuCN_Scanner_ScanAchivementAuto, startIndex, attempt + 1, counter + 1)
+  end
+end
+
+local function EnumerateTooltipStyledLines_new(tooltipData)
+  local texts = '';
+
+  local index = 0
+  for _, line in ipairs(tooltipData.lines) do
+    if index < 7 then
+      TooltipUtil.SurfaceArgs(line)
+    end
+      index = index + 1 
+  end
+
+  DevTools_Dump({ tooltipData })
+
+	return texts
+end
+
+function WoWeuCN_Scanner_ScanQuestAuto(startIndex, attempt, counter)
+  if (startIndex > 90000) then
+    return;
+  end
+  for i = startIndex, startIndex + 100 do
+    qcInformationTooltip:SetOwner(UIParent, "ANCHOR_NONE")
+    qcInformationTooltip:ClearLines()
+    qcInformationTooltip:SetHyperlink('quest:' .. i)
+    qcInformationTooltip:Show()
+    local text =  EnumerateTooltipLines(qcInformationTooltip)
+    if (text ~= '' and text ~= nil) then
+      WoWeuCN_Scanner_QuestToolTips[i .. ''] = text
+      print(i)
+    end
+  end
+  print(attempt)
+  print('index ' .. startIndex)
+  WoWeuCN_Scanner_QuestIndex = startIndex
+  if (counter >= 5) then
+     WoWeuCN_Scanner_wait(0.5, scanAuto, startIndex + 100, attempt + 1, 0)
+  else
+     WoWeuCN_Scanner_wait(0.5, scanAuto, startIndex, attempt + 1, counter + 1)
+  end
+end
+
+function WoWeuCN_Scanner_ScanCacheAuto(startIndex, attempt, counter)
+  if (startIndex > 30000 and startIndex < 60000) then
+    startIndex = 60000
+  end
+  if (startIndex > 80000) then
+    return;
+  end
+  if (counter == 0) then
+     print(startIndex)
+    end
+    
+    for i = startIndex, startIndex + 150 do
+     local title = C_QuestLog.GetQuestInfo(i)
+     if (title ~= '' and title ~= nil) then
+      print(title)
+     end
+    end
+    
+    WoWeuCN_Scanner_QuestIndex = startIndex
+    if (counter >= 5) then
+      WoWeuCN_Scanner_wait(0.2, WoWeuCN_Scanner_ScanCacheAuto, startIndex + 150, attempt + 1, 0)
+    else
+      WoWeuCN_Scanner_wait(0.2, WoWeuCN_Scanner_ScanCacheAuto, startIndex, attempt + 1, counter + 1)
+    end
+end
+
+local function EnumerateTooltipStyledLines_helper(...)
+  local texts = '';
+  local hasObjectivesSet = false
+    for i = 1, select("#", ...) do
+        local region = select(i, ...)
+        if region and region:GetObjectType() == "FontString" then
+      local text = region:GetText() -- string or nil
+			if (text ~= nil) then
+        if (text ~= " ")
+          then
+            text = "{{" .. text .. "}}"
+            local r, g, b, a = region:GetTextColor()
+            text = text .. "[[" .. r .. "]]" .. "[[" .. g .. "]]" .. "[[" .. b .. "]]"
+          end
+        print(i)
+        print(text)
+        texts = texts .. text	
+			end
+        end
+	end
+	return texts
+end
+
+local function EnumerateTooltipStyledLines(tooltip) -- good for script handlers that pass the tooltip as the first argument.
+  return EnumerateTooltipStyledLines_helper(tooltip:GetRegions())
+end
+
+function qcInformationTooltipSetup() -- *
+	qcInformationTooltip = CreateFrame("GameTooltip", "qcInformationTooltip", UIParent, "GameTooltipTemplate")
+	qcInformationTooltip:SetFrameStrata("TOOLTIP")
+end
