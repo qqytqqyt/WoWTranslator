96dfc42747b949a1bba8487751f4f6c8a9edd379
qqytqqyt <ytong007@hotmail.com>
2024-09-11 11:40:16 +0100

    [TWW] Updated item data after launch

--- WoWChinese/Retail/WoWeuCN_Tooltips/TooltipsData_cn.lua
+++ WoWChinese/Retail/WoWeuCN_Tooltips/TooltipsData_cn.lua
@@ -1,5 +1,5 @@
 ﻿WoWeuCN_Tooltips_base = "11.0.2.56311";   -- version
-WoWeuCN_Tooltips_date = "2024-08-26"; -- date of creation base
+WoWeuCN_Tooltips_date = "2024-08-28"; -- date of creation base
 
 WoWeuCN_Tooltips_lang = "CN";      -- language
 
