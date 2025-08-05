05a248b25d9237618a3a20f88e48539ba8e8cc5d
qqytqqyt <ytong007@hotmail.com>
2025-08-05 23:19:31 +0100

    [TWW] Updated data for 11.2.0

--- WoWChinese/Retail/WoWeuCN_Quests/QuestData.lua
+++ WoWChinese/Retail/WoWeuCN_Quests/QuestData.lua
@@ -1,5 +1,5 @@
-WoWeuCN_Quests_base = "11.1.5.60490";   -- version
-WoWeuCN_Quests_date = "2025-04-27"; -- date of creation base
+WoWeuCN_Quests_base = "11.2.0.62417";   -- version
+WoWeuCN_Quests_date = "2025-08-05"; -- date of creation base
 
 WoWeuCN_Quests_lang = "CN";      -- language
 
